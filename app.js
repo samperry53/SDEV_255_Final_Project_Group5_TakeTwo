9d4006580bdaee5b2f1f33f6ae4adabfb14a2a40
Sam Perry <sperry53@ivytech.edu>
2024-03-07 00:10:38 -0500

    modified code to mvc

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Course = require('./models/course');
 const { render } = require('ejs');
+const courseRoutes = require('./routes/courseRoutes');
 
 // express app
 const app = express();
@@ -16,117 +16,24 @@ mongoose.connect(dbURI)
 // register view engine
 app.set('view engine', 'ejs');
 
-// // listen for requests
-// app.listen(3000);
-
 // middleware and static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
 
-// // mongoose and mongo sandbox routes
-// app.get('/add-course', (req, res) => {
-//     const course = new Course({
-//         name: 'new course',
-//         description: 'a new course',
-//         subject: 'tutorial',
-//         credits: 3
-//     });
-
-//     course.save()
-//         .then((result) => {
-//             res.send(result)
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// })
-
-// app.get('/all-courses', (req,res) => {
-//     Course.find()
-//         .then((result) => {
-//             res.send(result);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// })
-
-// app.get('/single-course', (req, res) => {
-//     Course.findById('')
-//     .then((result) => {
-//         res.send(result);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-// })
-
-
-// app.get('/', (req, res) => {
-//     const courses = [
-//         {name: 'Biology', description: 'Science of Life', subject: 'Life Sciences', credits: '3'}
-//     ];
-//     res.render('index', { title: 'Home', courses });
-// });
-
-// course routes
 app.get('/', (req, res) => {
     res.redirect('/courses');
 });
 
-app.get('/courses/create', (req, res) => {
-    res.render('create', { title: 'Add A New Course'});
-})
-
-app.get('/courses', (req, res) => {
-    Course.find().sort({ createdAt: -1 })
-    .then((result) => {
-        res.render('index', { title: 'All Courses', courses: result })
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
-
-app.post('/courses', (req, res) => {
-    const course = new Course(req.body);
-
-    course.save()
-    .then((result) => {
-        res.redirect('/courses');
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
-
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    Course.findById(id)
-    .then(result => {
-       res.render('details', { course: result, title: 'Course Details' });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-})
-
-app.delete('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Course.findByIdAndDelete(id)
-    .then(result => {
-        res.json({ redirect: '/courses' })
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
+// course routes
+app.use('/courses', courseRoutes);
 
 
 // 404 page
 app.use((req, res) => {
-    res.render('404', { title: '404'});
+    res.status(404).render('404', { title: '404'});
 })
