9a5151725c395d4b00d6d7173cac42460495320a
Sam Perry <sperry53@ivytech.edu>
2024-03-06 22:28:47 -0500

    set some basic routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Course = require('./models/course');
 
 // express app
 const app = express();
@@ -14,12 +15,51 @@ mongoose.connect(dbURI)
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+// // listen for requests
+// app.listen(3000);
 
 // middleware and static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+// // mongoose and mongo sandbox routes
+// app.get('/add-course', (req, res) => {
+//     const course = new Course({
+//         name: 'new course',
+//         description: 'a new course',
+//         subject: 'tutorial',
+//         credits: 3
+//     });
+
+//     course.save()
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// })
+
+// app.get('/all-courses', (req,res) => {
+//     Course.find()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// })
+
+// app.get('/single-course', (req, res) => {
+//     Course.findById('')
+//     .then((result) => {
+//         res.send(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// })
+
 
 app.get('/', (req, res) => {
     const courses = [
@@ -27,6 +67,21 @@ app.get('/', (req, res) => {
     ];
     res.render('index', { title: 'Home', courses });
 });
+
+// course routes
+app.get('/', (req, res) => {
+    res.redirect('/courses');
+});
+
+app.get('/courses', (req, res) => {
+    Course.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render('index', { title: 'All Courses', courses: result })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 app.get('/courses/create', (req, res) => {
     res.render('create', { title: 'Add A New Course'});
