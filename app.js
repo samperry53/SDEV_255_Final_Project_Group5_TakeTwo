b6669c36adf257ebabfd1bbdaf4dde7279ddd6ea
Sam Perry <sperry53@ivytech.edu>
2024-03-07 22:32:03 -0500

    app not launching

--- app.js
+++ app.js
@@ -43,22 +43,3 @@ app.use(authRoutes);
 app.use((req, res) => {
     res.status(404).render('404', { title: '404'});
 })
-
-// cookies
-app.get('/set-cookies', (req, res) => {
-    
-    // res.setHeader('Set-Cookie', 'newUser=true');
-    res.cookie('newUser', false);
-    res.cookie('isEmployee', true { maxAge: 1000 * 60 * 60 * 24, secure: true, httpOnly: true });
-
-    res.send('you got the cookies!');
-});
-
-app.get('/read-cookies', (req, res) => {
-
-    const cookies = req.cookies;
-    console.log(cookies.newUser);
-
-    res.json(cookies);
-
-});
