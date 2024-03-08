adb602dd03320acec3c2e14873ce271008dde507
Sam Perry <sperry53@ivytech.edu>
2024-03-07 21:28:07 -0500

    cookie parser set up

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { render } = require('ejs');
 const courseRoutes = require('./routes/courseRoutes');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 // express app
 const app = express();
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.json());
+app.use(cookieParser);
 
 app.get('/', (req, res) => {
     res.redirect('/courses');
@@ -41,3 +43,22 @@ app.use(authRoutes);
 app.use((req, res) => {
     res.status(404).render('404', { title: '404'});
 })
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+    
+    // res.setHeader('Set-Cookie', 'newUser=true');
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true { maxAge: 1000 * 60 * 60 * 24, secure: true, httpOnly: true });
+
+    res.send('you got the cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+
+    const cookies = req.cookies;
+    console.log(cookies.newUser);
+
+    res.json(cookies);
+
+});
